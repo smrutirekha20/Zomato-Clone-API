ea341e61bd01249ed53b721d50c818a065926148
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-16 15:25:36 +0530

    project initialized,created zomato-clone-API

---
+++ src/main/java/com/example/zomato/ZomatoCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.zomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zomato/ZomatoCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
