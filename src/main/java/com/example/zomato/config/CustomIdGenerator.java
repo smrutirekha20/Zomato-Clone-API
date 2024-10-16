0b1ab1df46e8fb49c5f60cfa29ec1c9c1b80457b
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-16 17:55:30 +0530

    implemented custom id generator strategy

---
+++ src/main/java/com/example/zomato/config/CustomIdGenerator.java
@@ -0,0 +1,18 @@
+package com.example.zomato.config;
+
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.time.Year;
+import java.util.UUID;
+
+public class CustomIdGenerator implements IdentifierGenerator {
+
+
+    @Override
+    public Object generate(SharedSessionContractImplementor session, Object object) {
+        UUID uuid=UUID.randomUUID();
+        String id="zomato" + Year.now().toString()+uuid.toString();
+        return id;
+    }
+}
