d1587883f8c9e7d4073f4ff5d99ddc5820eb95c4
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-17 18:10:42 +0530

    mapping the request and response dtos

---
+++ src/main/java/com/example/zomato/mapper/RestaurantMapper.java
@@ -0,0 +1,30 @@
+package com.example.zomato.mapper;
+
+import com.example.zomato.entity.Restaurant;
+import com.example.zomato.responsedtos.RestaurantResponse;
+import com.example.zomato.requestdtos.RestaurantRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestaurantMapper {
+    public Restaurant mapToRestaurant(RestaurantRequest restaurantRequest, Restaurant restaurant){
+        restaurant.setName(restaurantRequest.getName());
+        restaurant.setDescription(restaurantRequest.getDescription());
+        restaurant.setEmail(restaurantRequest.getEmail());
+        restaurant.setPhoneNumber(restaurantRequest.getPhoneNumber());
+        restaurant.setDietTypes(restaurantRequest.getDietTypes());
+        return restaurant;
+    }
+    public RestaurantResponse mapToRestaurantResponse(Restaurant restaurant){
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+        restaurantResponse.setRestaurantId(restaurant.getRestaurantId());
+        restaurantResponse.setName(restaurant.getName());
+        restaurantResponse.setDescription(restaurant.getDescription());
+        restaurantResponse.setPhoneNumber(restaurant.getPhoneNumber());
+        restaurantResponse.setEmail(restaurant.getEmail());
+        restaurantResponse.setDietTypes(restaurant.getDietTypes());
+
+        return restaurantResponse;
+    }
+}
+
