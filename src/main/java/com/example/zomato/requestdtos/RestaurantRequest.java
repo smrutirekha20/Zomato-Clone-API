98ad8b2bb57a8f4c45e00874c5c76b33dc274b87
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-17 17:19:05 +0530

    create request and respose specificn DTOs

---
+++ src/main/java/com/example/zomato/requestdtos/RestaurantRequest.java
@@ -0,0 +1,32 @@
+package com.example.zomato.requestdtos;
+
+import com.example.zomato.enums.DietType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+public class RestaurantRequest {
+
+    @NotNull(message = "Name can not be null")
+    @NotBlank(message="Name can not be blank")
+    @Pattern(regexp = "^[A-Z][a-zA-Z0-9_]{2,30}$",message="username must be start with uppercase and must have underscore '_'")
+    private String name;
+
+    @NotBlank(message ="description can not be blank")
+    private String description;
+
+    @Pattern(regexp = "^(?:\\+91[-\\s]?|91[-\\s]?|0)?[789]\\d{9}$\n",message="number must be ten numerics")
+    private String phoneNumber;
+
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$",message="email must be end with @gmail.com")
+    private String email;
+
+    @NotNull(message="dietTypes can not be not null")
+    @NotBlank(message="dietTypes can not be blank")
+    private List<DietType> dietTypes;
+}
---
+++ src/main/java/com/example/zomato/responsedtos/RestaurantResponse.java
@@ -0,0 +1,20 @@
+package com.example.zomato.responsedtos;
+
+import com.example.zomato.enums.DietType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+public class RestaurantResponse {
+
+   private String restaurantId;
+   private String name;
+   private String description;
+   private String email;
+   private String phoneNumber;
+   private List<DietType> dietTypes;
+
+
+}
