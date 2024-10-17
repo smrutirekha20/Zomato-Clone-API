6b48a84d1e5356b18eb6cddf0cac4c74603f7ecd
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-17 18:13:59 +0530

    repository created , created response builder class and base url in yml

--- src/main/java/com/example/zomato/repository/RestaurantRepository.java
+++ src/main/java/com/example/zomato/repository/RestaurantRepository.java
@@ -1,4 +1,9 @@
 package com.example.zomato.repository;
 
-public class RestaurantRepository {
+import com.example.zomato.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant,String> {
 }
