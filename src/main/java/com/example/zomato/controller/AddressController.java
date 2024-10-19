7291c984308076f1c3d77526ede5d3b54335a233
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-19 15:50:32 +0530

    address is saved and update address

---
+++ src/main/java/com/example/zomato/controller/AddressController.java
@@ -0,0 +1,33 @@
+package com.example.zomato.controller;
+
+
+import com.example.zomato.requestdtos.AddressRequest;
+import com.example.zomato.responsedtos.AddressResponse;
+import com.example.zomato.service.AddressService;
+import com.example.zomato.utility.AppResponseBuilder;
+import com.example.zomato.utility.ErrorStructure;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("${zomato.base_url}")
+public class AddressController {
+
+    private AddressService addressService;
+    private AppResponseBuilder appResponseBuilder;
+
+    @PostMapping("/addresses")
+    public ResponseEntity<ErrorStructure<AddressResponse>> addRestaurant(@RequestBody AddressRequest addressRequest,@RequestParam String restaurantId) {
+       AddressResponse addressResponse=addressService.saveAddress(addressRequest,restaurantId);
+        return appResponseBuilder.success(HttpStatus.CREATED, "Address created", addressResponse);
+    }
+    @PutMapping("/addresses/{addressId}")
+    public ResponseEntity<ErrorStructure<AddressResponse>> updateRestaurant(@RequestBody AddressRequest addressRequest,@PathVariable String addressId) {
+        AddressResponse addressResponse=addressService.updateAddress(addressRequest,addressId);
+        return appResponseBuilder.success(HttpStatus.CREATED, "Address updated", addressResponse);
+    }
+
+}
