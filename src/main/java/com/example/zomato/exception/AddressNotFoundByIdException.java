a23c7dc9ec7b04c852511fb37ab17d933a176763
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-19 15:45:44 +0530

    exception  may occur so for handling purpose exception handler is used

---
+++ src/main/java/com/example/zomato/exception/AddressNotFoundByIdException.java
@@ -0,0 +1,12 @@
+package com.example.zomato.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class AddressNotFoundByIdException extends RuntimeException{
+
+    private final String message;
+}
