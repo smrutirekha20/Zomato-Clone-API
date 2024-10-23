aca6986bb25b4bbd62facbbeef3e124ea0c4cc0f
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-23 15:34:28 +0530

    adding the cuisines and findAll cuisines

---
+++ src/main/java/com/example/zomato/controller/CuisineController.java
@@ -0,0 +1,39 @@
+package com.example.zomato.controller;
+
+import com.example.zomato.requestdtos.AddressRequest;
+import com.example.zomato.requestdtos.CuisineRequest;
+import com.example.zomato.responsedtos.CuisineResponse;
+import com.example.zomato.service.CuisineService;
+import com.example.zomato.utility.AppResponseBuilder;
+import com.example.zomato.utility.ResponseStructure;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("${zomato.base_url}")
+public class CuisineController {
+
+    private final AppResponseBuilder appResponseBuilder;
+    private final CuisineService cuisineService;
+
+    @PostMapping("/cuisines/{restaurantId}")
+    public ResponseEntity<ResponseStructure<CuisineResponse>> addCuisine(@RequestBody @Valid CuisineRequest cuisineRequest, @PathVariable String restaurantId) {
+        CuisineResponse cuisineResponse = cuisineService.saveCuisine(cuisineRequest, restaurantId);
+        return appResponseBuilder.success(HttpStatus.CREATED, "Cuisine created", cuisineResponse);
+    }
+
+    @GetMapping("/cuisines")
+    public ResponseEntity<ResponseStructure<List<CuisineResponse>>> getCuisines(){
+        List<CuisineResponse> cuisineResponse = cuisineService.getAllCuisines();
+        return appResponseBuilder.success(HttpStatus.CREATED, "Cuisine created", cuisineResponse);
+    }
+}
+
