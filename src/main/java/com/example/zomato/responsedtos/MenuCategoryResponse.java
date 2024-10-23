87f181a6283d7bc2bace764d999f85fd2a32d224
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-23 23:14:40 +0530

    Request and response dto created

---
+++ src/main/java/com/example/zomato/responsedtos/MenuCategoryResponse.java
@@ -0,0 +1,12 @@
+package com.example.zomato.responsedtos;
+
+import lombok.Data;
+
+@Data
+public class MenuCategoryResponse {
+
+    private String menuCategoryId;
+    private String title;
+
+
+}
