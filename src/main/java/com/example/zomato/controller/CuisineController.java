cd8d763f9839b5dd0b9d9e1ef3ecc63c2b825598
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-23 23:10:34 +0530

    changed controller to RestController

--- src/main/java/com/example/zomato/controller/CuisineController.java
+++ src/main/java/com/example/zomato/controller/CuisineController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("${zomato.base_url}")
 public class CuisineController {
