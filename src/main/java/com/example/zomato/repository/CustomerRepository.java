0ca5ef6731d6dfec2b7e55653cb174881c5b4d55
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-25 16:36:05 +0530

    created repository for customer,user,restaurant owner

---
+++ src/main/java/com/example/zomato/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.zomato.repository;
+
+import com.example.zomato.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,String> {
+}
---
+++ src/main/java/com/example/zomato/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.zomato.repository;
+
+import com.example.zomato.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}
