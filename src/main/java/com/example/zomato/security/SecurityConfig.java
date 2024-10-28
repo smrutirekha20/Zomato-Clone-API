0c0968079aa546afec6eae438708ffc21b5327a0
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-10-28 14:54:48 +0530

    providing authentication to the user

--- src/main/java/com/example/zomato/security/SecurityConfig.java
+++ src/main/java/com/example/zomato/security/SecurityConfig.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -29,6 +32,19 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
 
         return provider;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(authorize->authorize
+                        .requestMatchers("api/v1/register","api/v1/login","api/v1/logout")
+                        .permitAll()
+                       // .requestMatchers("api/v1/restaurants/**").hasRole("RESTAURANT_OWNER")
+                        .anyRequest().authenticated())
+                .authenticationProvider(this.authenticationProvider())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
 }
