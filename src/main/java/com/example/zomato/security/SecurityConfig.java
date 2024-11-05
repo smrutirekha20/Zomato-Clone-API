b09e77411535af695f1fd6dfa516cb08a821fce9
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-11-05 14:28:34 +0530

    no changes happen

--- src/main/java/com/example/zomato/security/SecurityConfig.java
+++ src/main/java/com/example/zomato/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.zomato.security;
 
+import com.example.zomato.securityfilters.AuthFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -24,6 +26,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final JWTService jwtService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -49,6 +52,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .authenticationProvider(this.authenticationProvider())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new AuthFilter(jwtService),UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -56,5 +60,6 @@ public class SecurityConfig {
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
 
 }
