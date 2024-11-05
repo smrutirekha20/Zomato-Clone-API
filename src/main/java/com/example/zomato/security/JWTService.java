949583a953f7c31cc1c98ed49ac41e576f76ecd7
smrutirekha20 <dassmrutirekha20@gmail.com>
2024-11-05 14:25:45 +0530

    modifying authorities null to authorities based on the user's role

--- src/main/java/com/example/zomato/security/JWTService.java
+++ src/main/java/com/example/zomato/security/JWTService.java
@@ -1,5 +1,6 @@
 package com.example.zomato.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -17,9 +18,9 @@ public class JWTService {
     @Value("${zomato.jwt.secret}")
     private String secret;
 
-    public String generateJWT(String username,long expiringDuration){
+    public String generateJWT(String username,long expiringDuration,String role){
         return Jwts.builder()
-                .setClaims(Map.of())
+                .setClaims(Map.of("role",role))
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+expiringDuration))
@@ -29,4 +30,29 @@ public class JWTService {
     private Key getSigningKey(){
        return Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
     }
+
+    public Claims extractClaims(String token){
+       return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsername(Claims claims){
+        return claims.getSubject();
+    }
+
+    public Date getIssuedAt(Claims claims){
+        return claims.getIssuedAt();
+    }
+    public Date getExpiringDate(Claims claims){
+        return claims.getExpiration();
+    }
+
+    public String getRole(Claims claims){
+        return claims.get("role",String.class);
+    }
+
+
 }
--- src/main/java/com/example/zomato/securityfilters/AuthFilter.java
+++ src/main/java/com/example/zomato/securityfilters/AuthFilter.java
@@ -1,5 +1,6 @@
 package com.example.zomato.securityfilters;
 
+import com.example.zomato.enums.UserRole;
 import com.example.zomato.security.JWTService;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
@@ -7,14 +8,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @AllArgsConstructor
+@Slf4j
 public class AuthFilter extends OncePerRequestFilter {
 
 
@@ -24,15 +29,27 @@ public class AuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("Authorization");
         if (token != null) {
+            log.info("Authenticating Token...");
             token = token.substring(7);
 
             Claims claims = jwtService.extractClaims(token);
             String username = jwtService.getUsername(claims);
+            String role= jwtService.getRole(claims);
+
 
             if (username != null) {
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, null, null);
+                log.info("Username extracted");
+                //UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority(role)));
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                        = new UsernamePasswordAuthenticationToken(username, null, UserRole.valueOf(role)
+                                                                                                   .getPrivileges()
+                                                                                                   .stream()
+                                                                                                   .map(privilege -> new SimpleGrantedAuthority(privilege.name()))
+                                                                                                    .toList());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                log.info("Token Authentication successful.");
             }
         }
 
